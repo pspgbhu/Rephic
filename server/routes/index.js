9270f065401644f23d1c6ac51c7ba3fccc59043d
pspgbhu <brotherchun001@gmail.com>
2018-04-24 18:17:36 +0800

    fix: 页面路由正则匹配规则修正

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const router = require('koa-router')();
 const viewsRoutes = require('./views');
 const apiRoutes = require('./api');
 
-router.use(apiRoutes.routes());
 router.use(viewsRoutes.routes());
+router.use(apiRoutes.routes());
 
 module.exports = router;
